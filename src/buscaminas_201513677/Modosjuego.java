044ce8fdb1c1aeb5cf7d842ea3b7f251070c3dad
Dany <Dany@DESKTOP-AF2J7VE>
2017-08-15 16:17:31 -0600

    Commit 1 Menus

---
+++ src/buscaminas_201513677/Modosjuego.java
@@ -0,0 +1,25 @@
+
+package buscaminas_201513677;
+/**
+ *
+ * @author Dany
+ */
+public class Modosjuego {
+    
+    
+    
+    public void Principiante() {
+        System.out.println("hola");
+    }
+
+    public void Intermedio() {
+        System.out.println("hoola");
+    }
+
+    public void Experto() {
+        System.out.println("holaaa");
+    }
+            
+        }
+    
+
