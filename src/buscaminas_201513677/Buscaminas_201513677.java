86f5e9c17297f54d44d874b3d231bbe60baaa695
Dany <Dany@DESKTOP-AF2J7VE>
2017-08-15 16:16:02 -0600

    Commit 1 Menus

--- src/buscaminas_201513677/Buscaminas_201513677.java
+++ src/buscaminas_201513677/Buscaminas_201513677.java
@@ -15,7 +15,8 @@ public class Buscaminas_201513677 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Menu menu1 = new Menu();
     }
     
 }
