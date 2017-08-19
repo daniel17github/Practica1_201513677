09b2848f13b85817fbb79963a642d23484278110
Dany <Dany@DESKTOP-AF2J7VE>
2017-08-19 17:58:17 -0600

    6

--- src/buscaminas_201513677/Modosjuego.java
+++ src/buscaminas_201513677/Modosjuego.java
@@ -4,33 +4,70 @@ package buscaminas_201513677;
  *
  * @author Dany
  */
-public class Modosjuego {
+    public class Modosjuego {
     
+  
     
+    public Modosjuego(){
+ 
+}
     
-    public static void Principiante() {
-        System.out.println("---------------------------------");
-        System.out.println("       NIVEL PRINCIPIANTE        ");
+    //Nivel principiante
+    public static  void Principiante() {
+        int matriz[][]= new int[4][4];
         
-             
         
+        System.out.println("    ---------------------------------");
+        System.out.println("           NIVEL PRINCIPIANTE        ");
+      
+        for (int x=0; x < matriz.length; x++) {
+         System.out.print("|");
+            for (int y=0; y < matriz[x].length; y++) {
+                System.out.print (matriz[x][y]);
+        if (y!=matriz[x].length-1) System.out.print("\t");
+            }
+                System.out.println("|");
+            }
+    }
+    
+    
+// Nivel Intermedio
+    public  static void Intermedio() {
+         int matriz[][]= new int[6][6];
+        
+        
+        System.out.println("    ---------------------------------");
+        System.out.println("           NIVEL INTERMEDIO          ");
+        
+        for (int x=0; x < matriz.length; x++) {
+         System.out.print("|");
+            for (int y=0; y < matriz[x].length; y++) {
+                System.out.print (matriz[x][y]);
+        if (y!=matriz[x].length-1) System.out.print("\t");
+            }
+                System.out.println("|");
+            }         
     }
 
-    public static void Intermedio() {
-        System.out.println("---------------------------------");
-        System.out.println("       NIVEL INTERMEDIO          ");
-    }
-
     
     
+//Nivel Experto    
     public static void Experto() {
-        System.out.println("---------------------------------");
-        System.out.println("         NIVEL EXPERTO          ");
-    }
+        
+        int matriz [][]= new int[8][8];
+        System.out.println("    --------------------------------");
+        System.out.println("             NIVEL EXPERTO          ");
+    
             
-    
-    
-    
+    for (int x=0; x < matriz.length; x++) {
+         System.out.print("|");
+            for (int y=0; y < matriz[x].length; y++) {
+                System.out.print (matriz[x][y]);
+        if (y!=matriz[x].length-1) System.out.print("\t");
+            }
+                System.out.println("|");
+            }
+
         }
     
-
+    }
