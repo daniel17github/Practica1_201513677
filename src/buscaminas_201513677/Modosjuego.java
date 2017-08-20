ad968046db47cd7f82f1108c47e3aa196ecdc2c8
Dany <Dany@DESKTOP-AF2J7VE>
2017-08-20 00:36:28 -0600

    6

--- src/buscaminas_201513677/Modosjuego.java
+++ src/buscaminas_201513677/Modosjuego.java
@@ -1,5 +1,8 @@
 
 package buscaminas_201513677;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 /**
  *
  * @author Dany
@@ -34,8 +37,12 @@ package buscaminas_201513677;
 // Nivel Intermedio
     public  static void Intermedio() {
          int matriz[][]= new int[6][6];
+         int opc1 = 0;
         
-        
+         do{
+             try{
+                 
+                 Scanner b = new Scanner(System.in);
         System.out.println("    ---------------------------------");
         System.out.println("           NIVEL INTERMEDIO          ");
         
@@ -46,12 +53,43 @@ package buscaminas_201513677;
         if (y!=matriz[x].length-1) System.out.print("\t");
             }
                 System.out.println("|");
-            }         
+            }
+        System.out.println("------------------");
+        System.out.println(" Voltear:   V");
+        System.out.println(" Reiniciar: R");
+        System.out.println(" Salir:     S"); 
+        System.out.println("");
+        System.out.println("Ingresar Opcion:");
+        opc1 = b.nextInt();
+        System.out.println("   ---------------------------------");
+        switch(opc1){
+         
+             case 1:
+                    System.out.println("hola");
+                 break;
+         
+             case 2:
+                 System.out.println("holix");
+                 break;
+                 
+             case 3:
+                 System.out.println("Saliendo....Regreso a Menu Principal");
+                 break;
+         
+                  
+         }
+             }catch (InputMismatchException e) {
+                    System.err.println("Error!! solo se puden ingresar datos del tipo numerico");
+                    System.err.println("Intentelo nuevamente");
+                     
+                } catch (Exception e) {
+                    System.out.println("ERROR !!!");
+                }
+         } while (opc1!=3);   
     }
 
     
-    
-//Nivel Experto    
+    //Nivel Experto    
     public static void Experto() {
         
         int matriz [][]= new int[8][8];
@@ -69,5 +107,8 @@ package buscaminas_201513677;
             }
 
         }
+    
+    
+      
     
     }
